6e8630925bae59323cb74fcf9f34d018f4a55f53
João Saraiva <32929768+Saraiv@users.noreply.github.com>
2023-11-30 18:35:22 +0000

    Restarting animations, added more sprites and prefabs?

--- Scripts/Nico/Movement.gd
+++ Scripts/Nico/Movement.gd
@@ -5,33 +5,30 @@ export(int) var run_speed = 90
 var animationPlayer = null
 var animationTree = null
 var animationState = null
+var sprite = null
+var scale_changed = false
 
 func _ready():
 	animationPlayer = $AnimationPlayer
 	animationTree = $AnimationTree
-	animationState = animationTree.get("parameters/playback") 
-	animationTree.active = true
+	sprite = $Sprite
 
 func _physics_process(delta):
 	var velocity = Vector2.ZERO
 
 	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1.0
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1.0
+		velocity.x = 1.0
+	elif Input.is_action_pressed("ui_left"):
+		velocity.x = -1.0
 
+	if(velocity.x > 0):
+		get_node(".").scale.x = scale.y * 1
+	elif(velocity.x < 0):
+		get_node(".").scale.x = scale.y * -1
+
+	position += velocity * delta
 
 	velocity = velocity.normalized()
-
-	if velocity == Vector2.ZERO:
-		# Idle
-		animationState.travel("Idle")
-		velocity = Vector2.ZERO
-	else:
-		# Walk
-		animationTree.set("parameters/Idle/blend_position", velocity)
-		animationTree.set("parameters/Walk/blend_position", velocity)
-		animationState.travel("Walk")
 
 	if Input.is_action_pressed("ui_run"):
 		move_and_slide(velocity * run_speed)
